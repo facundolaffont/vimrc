52d114a951d41c7cecb1a16a31ba8d5ec5f74c9c
Facundo Laffont <facundolaffont@gmail.com>
2023-04-16 21:34:51 -0300

    Updated install.sh
    - Now it doesn't write vimrc when curl fails downloading it.

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 echo "Installing for user $USER:"
 if [[ ! -a $HOME/.vim/vimrc ]]
 then
-    curl -LsS https://raw.githubusercontent.com/facundolaffont/vimrc/main/vimrc > $HOME/.vim/vimrc
+    curl -fLSso $HOME/.vim/vimrc https://raw.githubusercontent.com/facundolaffont/vimrc/main/vimrc
     echo -e "\t- vimrc was copied."
 else
-    curl -LsS https://raw.githubusercontent.com/facundolaffont/vimrc/main/vimrc > $HOME/.vim/vimrc
+    curl -fLSso $HOME/.vim/vimrc https://raw.githubusercontent.com/facundolaffont/vimrc/main/vimrc
     echo -e "\t- vimrc was overwritten."
 fi
 
 if [[ ! -a $HOME/.vim/autoload/plug.vim ]]
 then
-    curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
+    curl -fLSso $HOME/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     echo -e "\t- VimPlug was installed."
 else
